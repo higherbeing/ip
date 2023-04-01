616466860a729eedbd75c9d39ddd89c29d845eca
higherbeing <sreekanth.exe@gmail.com>
2023-04-01 11:26:22 -0400

    Update index.py

--- index.py
+++ index.py
@@ -1,7 +1,28 @@
-from http.server import BaseHTTPRequestHandler
-class handler(BaseHTTPRequestHandler):
-  def do_GET(self):
-    self.send_response(200)
-    self.end_headers()
-    self.wfile.write(self.headers.get('x-forwarded-for').encode())
-    return
+!pip install Flask-APScheduler
+
+from flask import Flask
+from flask_apscheduler import APScheduler
+
+sched = APScheduler()
+
+app = Flask(__name__)
+
+def job1():
+  print('hello3')
+
+
+def hello_world():
+    return 'Hello, World!'
+
+if __name__ == '__main__':
+  sched.add_job(id='job1',func=job1,trigger='interval',seconds=5)
+  sched.start()
+  app.run(debug=True,use_reloader=False)
+  
+#from http.server import BaseHTTPRequestHandler
+#class handler(BaseHTTPRequestHandler):
+#  def do_GET(self):
+#    self.send_response(200)
+#    self.end_headers()
+#    self.wfile.write(self.headers.get('x-forwarded-for').encode())
+#    return
